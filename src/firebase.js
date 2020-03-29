1745dd8818dd8655bb7a4e420aab97555905af15
haichew <42407833+haichew@users.noreply.github.com>
2020-03-29 11:43:48 +0800

    .

--- src/firebase.js
+++ src/firebase.js
@@ -2,10 +2,14 @@ import firebase from 'firebase';
 
 const firebaseConfig = {
     //Key in your database details
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: ""
+    apiKey: "AIzaSyDvd7ML2qT9L6VG2acFiP-1MFJglPjrgZE",
+    authDomain: "bt3103wk8-17460.firebaseapp.com",
+    databaseURL: "https://bt3103wk8-17460.firebaseio.com",
+    projectId: "bt3103wk8-17460",
+    storageBucket: "bt3103wk8-17460.appspot.com",
+    messagingSenderId: "16312163534",
+    appId: "1:16312163534:web:339e9e533895ca1f4a049f",
+    measurementId: "G-LM2CFKFHXR"
 
   };
   
